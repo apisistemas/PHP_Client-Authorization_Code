d37afdff8059c5b18b566f9fdf53c487c835ab1d
apisistemas <api@info.ufrn.br>
2016-03-17 10:25:27 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,33 +8,33 @@ require('Client.php');
 require('GrantType/IGrantType.php');
 require('GrantType/AuthorizationCode.php');
 
-const CLIENT_ID     = 'SEU_CLIENT_ID';
-const CLIENT_SECRET = 'SEU_CLIENT_SECRET';
+const CLIENT_ID     = 'client_id';
+const CLIENT_SECRET = 'client_secret';
 
-const REDIRECT_URI           = 'URL_DE_RETORNO';
-const AUTHORIZATION_ENDPOINT = 'URL_DE_AUTORIZACAO_DO_OAUTH';
-const TOKEN_ENDPOINT         = 'URL_DE_RECUPERACAO_DE_TOKEN_DO_OAUTH';
+const REDIRECT_URI           = 'http://localhost/my-php-app';
+const AUTHORIZATION_ENDPOINT = 'http://apitestes.info.ufrn.br/authz-server/oauth/authorize';
+const TOKEN_ENDPOINT         = 'http://apitestes.info.ufrn.br/authz-server/oauth/token';
 
 $client = new OAuth2\Client(CLIENT_ID, CLIENT_SECRET);
 
 if (!isset($_GET['code']))
 {
-	// RECUPERANDO O "AUTHORIZATION_CODE"
+    // RECUPERANDO O "AUTHORIZATION_CODE"
     $auth_url = $client->getAuthenticationUrl(AUTHORIZATION_ENDPOINT, REDIRECT_URI);
     header('Location: ' . $auth_url);
     die('Redirect');
 }
 else
 {
-	// RECUPERANDO O "ACCESS_TOKEN"
+    // RECUPERANDO O "ACCESS_TOKEN"
     $params = array('code' => $_GET['code'], 'redirect_uri' => REDIRECT_URI);
     $response = $client->getAccessToken(TOKEN_ENDPOINT, 'authorization_code', $params);
 	
-	// INFORMANDO O "ACCESS_TOKEN" RCUPERADO AO CLIENTE
+    // INFORMANDO O "ACCESS_TOKEN" RCUPERADO AO CLIENTE
     $client->setAccessToken($response['result']['access_token']);
 	
-	// CONSUMINDO SERVIÇO
-    $response = $client->fetch('URL_DE_SERVICO_REST');
+    // CONSUMINDO SERVIÇO
+    $response = $client->fetch('http://apitestes.info.ufrn.br/ensino-services/services/consulta/listavinculos/usuario');
 
     // MOSTRANDO RETORNO DO SERVIÇO
     var_dump($response, $response['result']);
